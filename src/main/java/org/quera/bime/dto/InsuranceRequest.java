0cfc7c2a5c218f799b6b0c18ddaa0e10cc4e5aaa
a.parvaneh <a.parvaneh@middleeastbank.ir>
2023-04-18 14:41:05 +0330

    dev-insurance-v1

--- src/main/java/org/quera/bime/dto/InsuranceRequest.java
+++ src/main/java/org/quera/bime/dto/InsuranceRequest.java
@@ -1,7 +1,9 @@
 package org.quera.bime.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.quera.bime.model.Type;
 
 import java.io.Serializable;
 
@@ -9,4 +11,14 @@ import java.io.Serializable;
 @Setter
 public class InsuranceRequest implements Serializable {
 
+    @JsonProperty(value = "type")
+    private Type type;
+    @JsonProperty(value = "name")
+    private String name;
+    @JsonProperty(value = "price")
+    private Double price;
+    @JsonProperty(value = "company_id")
+    private Long company_id;
+    @JsonProperty(value = "min_age")
+    private Integer integer;
 }
